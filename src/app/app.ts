83a112889ac7eabec72fa2ddeee18c079d8ee33d
Ivan <mi55737@zut.edu.pl>
2026-08-17 17:40:45 +0200

    defined main methods and variables

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,43 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NgIf } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone : true,
+  imports: [ NgIf, FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('mock-interview');
+
+  // 1. Variables (The State)
+   imageSrc : string | ArrayBuffer | null = null;
+   extractedText : string = ' ';
+   isLoading : boolean = false;
+   selectedFile : File | null = null;
+
+   // 2. Methods (The Actions)
+
+   onFileSelected(sevent: any) {};
+  
+   //Logic to read the selected file and ImageSrc
+
+   
+
+
+
+   extractText() {
+
+    this.isLoading = true;
+
+   };
+
+
+   copyToClipboard() {};
+
+
+
+
 }
